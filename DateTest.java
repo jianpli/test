3842da8dea83a66ae454d259a9bc0acebf1d4222
jianpli <jianpli@redhat.com>
2017-07-24 15:20:20 +0800

    test

---
+++ DateTest.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+/**
+ * Created by jianpli on 7/24/17.
+ */
+public class DateTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Date());
+    }
+}
+
